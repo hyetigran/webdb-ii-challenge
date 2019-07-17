14fb6899d09efe2a50ba3ee1192b6eaf84bfbf5c
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-07-17 18:28:06 +0400

    add update route

--- routes/carRoutes.js
+++ routes/carRoutes.js
@@ -13,3 +13,13 @@ router.get("/", (req, res) => {
       res.status(500).json({ err });
     });
 });
+
+router.put("/:id", (req, res) => {
+  Car.update(req.params.id, req.body)
+    .then(result => {
+      res.status(200).json({ cars: result });
+    })
+    .catch(err => {
+      res.status(500).json({ err });
+    });
+});
