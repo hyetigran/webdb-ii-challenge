80dd36ed4047019cf1436967f36615f50176adc7
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-07-17 18:15:14 +0400

    add car model

--- models/car.js
+++ models/car.js
@@ -1,1 +1,21 @@
 const db = require("./database.db3");
+
+module.exports = {
+  get() {
+    return db("cars");
+  },
+
+  async update(id, data) {
+    const count = await db("cars")
+      .where("id", id)
+      .update(data);
+    return count > 0 ? this.get(id) : null;
+  },
+
+  async remove(id) {
+    const count = await db("cars")
+      .where("id", id)
+      .del();
+    return count;
+  }
+};
